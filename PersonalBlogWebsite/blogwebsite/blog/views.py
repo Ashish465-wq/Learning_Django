a2f0536f6a8db11c5252de65cb41615f77898247
Priyanshu Arya <priyanshuarya2482000@gmail.com>
2024-08-21 15:53:32 +0530

    Completed

--- PersonalBlogWebsite/blogwebsite/blog/views.py
+++ PersonalBlogWebsite/blogwebsite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Post, Comment
+from .forms import CommentForm, PostForm
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -39,9 +41,33 @@ def post_create(request):
             
 @login_required
 def post_edit(request, post_id):
-    pass
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', post_id= post.id)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form})
+
 
 
 @login_required
 def post_delete(request, post_id):
-    pass
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
